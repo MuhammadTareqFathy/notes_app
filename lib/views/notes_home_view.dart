648dfb4e8afd2bb17a1fa2587561fc48dfbd1591
MuhammadTareqFathy <muhammad.elbishihi@gmail.com>
2024-07-06 18:16:46 +0300

    create appBar Icon

--- lib/views/notes_home_view.dart
+++ lib/views/notes_home_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_app_bar_icon.dart';
 
 class NotesHomeView extends StatelessWidget {
   const NotesHomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+        actions: const [
+          CustomAppBarIcon(),
+        ],
+      ),
+    );
   }
 }
